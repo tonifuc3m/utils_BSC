337dcb475f0d303555892308e6aa3eec47cbc8a0
antonio miranda <antonio.miranda@alumnos.uc3m.es>
2021-02-16 14:51:02 +0100

    add correct Brat parsing (no strip issues) and parse_tsv()

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 14 14:57:08 2020
+Created on Mon Jun  1 15:25:28 2020
 
 @author: antonio
 """
@@ -18,29 +18,23 @@ def parse_ann(datapath, labels_to_ignore = [], with_notes=False):
     ----------
     datapath: str. 
         Route to the folder where the files are. 
-    labels_to_ignore: list
-        List of annotation labels I will not parse
-    with_notes: bool
-        Flag. Whether to parse AnnotatorNotes or not
            
     Returns
     -------
     df: pandas DataFrame 
         It has information from ann files. Columns: 'annotator', 'bunch',
-        'filename', 'mark', 'label', 'offset1', 'offset2', 'span', 'code', 
-        'offset'
+        'filename', 'mark', 'label', 'offset1', 'offset2', 'span', 'code'
     '''
     start = time.time()
     info = []
-
     ## Iterate over the files and parse them
     filenames = []
+    print(datapath)
     for root, dirs, files in os.walk(datapath):
-         for filename in files:
-             if filename[-3:] != 'ann':
-                 continue
-                 
-             info = parse_one_ann(info, filenames, root, filename, labels_to_ignore,
+        for filename in files:
+            if filename[-3:] != 'ann':
+                continue
+            info = parse_one_ann(info, filenames, root, filename, labels_to_ignore,
                                   ignore_related=True, with_notes=with_notes)
              
     if with_notes == True:
@@ -51,8 +45,6 @@ def parse_ann(datapath, labels_to_ignore = [], with_notes=False):
                                      'label','offset1','offset2','span'])
     end = time.time()
     print("Elapsed time: " + str(round(end-start, 2)) + 's')
-    
-    df['offset'] = df[['offset1', 'offset2']].agg(' '.join, axis=1)
     
     return df
 
@@ -80,12 +72,12 @@ def parse_one_ann(info, filenames, root, filename, labels_to_ignore,
         splitted = line.split('\t')
         if len(splitted)<3:
             print('Line with less than 3 tabular splits:')
-            print(os.path.join(root, filename))
+            print(root + filename)
             print(line)
             print(splitted)
         if len(splitted)>3:
             print('Line with more than 3 tabular splits:')
-            print(os.path.join(root, filename))
+            print(root + filename)
             print(line)
             print(splitted)
         mark = splitted[0]
@@ -95,11 +87,11 @@ def parse_one_ann(info, filenames, root, filename, labels_to_ignore,
         if label in labels_to_ignore:
             continue
         offset = label_offset[1:]
-        span = splitted[2].strip()
+        span = splitted[2].strip('\n')
         if with_notes==False:
             info.append([annotator, bunch, filename,mark, label,
                          offset[0], offset[-1], 
-                         span.strip(string.punctuation)])
+                         span.strip('\n')])
             continue
         
         if mark in mark2code.keys():
@@ -108,6 +100,32 @@ def parse_one_ann(info, filenames, root, filename, labels_to_ignore,
             code = ''
         info.append([annotator, bunch, filename,mark, label,
                      offset[0], offset[-1], 
-                     span.strip(string.punctuation), code])
+                     span.strip('\n'), code])
             
     return info
+
+def parse_tsv(in_path):
+    '''
+    DESCRIPTION: Get information from ann that was already stored in a TSV file.
+    
+    Parameters
+    ----------
+    in_path: string
+        path to TSV file with columns: ['annotator', 'bunch', 'filename', 
+        'mark','label', 'offset1', 'offset2', 'span', 'code']
+        Additionally, we can also have the path to a 3 column TSV: ['code', 'label', 'span']
+    
+    Returns
+    -------
+    df_annot: pandas DataFrame
+        It has 4 columns: 'filename', 'label', 'code', 'span'.
+    '''
+    df_annot = pd.read_csv(in_path, sep='\t', header=None)
+    if len(df_annot.columns) == 8:
+        df_annot.columns=['annotator', 'bunch', 'filename', 'mark',
+                      'label', 'offset1', 'offset2', 'span', 'code']
+    else:
+        df_annot.columns = ['code', 'span', 'label']
+        #df_annot['label'] = 'MORFOLOGIA_NEOPLASIA'
+        df_annot['filename']  ='xx'
+    return df_annot
